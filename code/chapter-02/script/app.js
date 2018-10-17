b6bcbbdca54af5e4bd58863701ad83c45a21c821
liuruoran88 <liuruoran88@163.com>
2018-10-17 15:41:43 +0800

    Track 6 files into repository.
    - modified code/chapter-02/script/app.js
    - modified code/chapter-02/index.html
    - untracked code/chapter-02/assets/mockData/homeList.json
    - untracked code/chapter-02/assets/mockData/detail.json
    - untracked code/chapter-02/detail.html
    - untracked code/chapter-02/readme.md
    
    Discard 1 files from repository.
    
    - untracked code/chapter-02/server.js
    
    Auto commit by GitBook Editor

--- code/chapter-02/script/app.js
+++ code/chapter-02/script/app.js
@@ -1,151 +1,134 @@
 /**
- *  app.js 页面jsx相关处理
- * 
- * 
+ *  app.js 主页面js，事件绑定和处理
  */
 
-(function () {
+'use strict'
 
-	'use strict'
-
-	var app = {
-		sidebarShow: false,
-		loadingShow: true,
-		homeListTemplate: document.querySelector('.main-list-item')
-	};
-
-	var initialHomeListData = {
-		"status": 0,
-		"errMsg": "",
-		"data": {
-			"list": [{
-				"img": "https://boscdn.baidu.com/assets/pwabook/img/applewatch.jpg",
-				"name": "Apple Smartwatches",
-				"price": 300,
-				"description": "Swimproof|Alitmeter|GPS"
-			}]
-		}
-	};
-
+var app = {
+	sidebarShow: false,
+	loadingShow: true,
+	homeListTemplate: document.querySelector('.main-list-item'),
+	detailTemplate: document.querySelector('.main-detail')
+};
 
 /*******************
- * 事件监听
- *******************/
-	// sidebar btn
-	document.querySelector('.icon-category').addEventListener('click', function () {
-		app.openSidebar();
-	})
+* 页面
+*******************/
 
-	document.querySelector('.sidebar').addEventListener('click', function () {
-		app.closeSidebar();
-	})
+app.refresh = function () {
+	var url = './assets/mockData/homelist.json';
+	app.getData(url, 'homeListTemplate');
+}
 
-	document.querySelector('.mask').addEventListener('click', function () {
-		app.closeSidebar();
-	})
-
-
-	document.querySelector('.icon-refresh').addEventListener('click', function () {
-		app.refresh();
-	})
-
-
-/*******************
- * 函数方法
- *******************/
-
-	app.refresh = function () {
-		var url = 'https://boscdn.baidu.com/assets/pwabook/mock/homelist/mockData.json';
-  	app.getData(url, 'homeListTemplate');
-	}
-
-	app.getData = function(url, templateName) {
-		app.showLoading();
-    // TODO add cache logic here
-    if ('caches' in window) {
-      /*
-       * Check if the service worker has already cached this data
-       * data. If the service worker has the data, then display the cached
-       * data while the app fetches the latest data.
-       */
-      caches.match(url).then(function(response) {
-        if (response) {
-          response.json().then(function updateFromCache(json) {
-            var results = json.data.list;
-            app.hideLoading();
-            app.updateTemplate(results, templateName);
-          });
-        }
-      });
+app.getData = function(url, templateName) {
+	app.showLoading();
+  // // TODO add cache logic here
+  // if ('caches' in window) {
+  //   /*
+  //    * Check if the service worker has already cached this data
+  //    * data. If the service worker has the data, then display the cached
+  //    * data while the app fetches the latest data.
+  //    */
+  //   caches.match(url).then(function(response) {
+  //     if (response) {
+  //       response.json().then(function updateFromCache(json) {
+  //         var results = json.data.list;
+  //         app.hideLoading();
+  //         app.updateTemplate(results, templateName);
+  //       });
+  //     }
+  //   });
+  // }
+  // Fetch the latest data.
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.response);
+      var results = response.data.list;
+      app.updateTemplate(results, templateName);
     }
-    // Fetch the latest data.
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-        if (request.status === 200) {
-          var response = JSON.parse(request.response);
-          var results = response.data.list;
-          app.updateTemplate(results, templateName);
-        }
-      } else {
-        // Return the initial home list since no data is available.
-        app.updateTemplate(initialHomeListData.data.list, templateName);
-      }
-    };
-    request.open('GET', url);
-    request.send();
+    else {
+      console.error('Get data fail')
+    }
   };
+  xhr.open('GET', url);
+  xhr.send();
+};
 
-	/**
-	 * [updateTemplate 更新页面
-	 * @param  {[type]} data         获取的数据
-	 * @param  {[type]} templateName 数据对应模板名称
-	 * @return {[type]}              [description]
-	 */
-  app.updateTemplate = function (data, templateName) {
-    app.hideLoading();
-  	if (templateName === 'homeListTemplate') {
-  		var container = document.querySelector('.main-list')
-  		container.textContent = '';
-  		data.forEach(function (item) {
-  			var tpl = app.homeListTemplate.cloneNode(true);
-  			tpl.querySelector('a').setAttribute('href', item.url);
-  			tpl.querySelector('img').setAttribute('src', item.img);
-  			tpl.querySelector('.name').textContent = item.name;
-  			tpl.querySelector('.price').textContent = '$' + item.price;
-  			tpl.querySelector('.decs').textContent = item.description;
-  			container.appendChild(tpl);
-  		})
-  	}
-  }
-
-	app.openSidebar = function () {
-		if (app.sidebarShow) return;
-		document.querySelector('.mask').classList.remove('hide');
-		document.querySelector('.sidebar').classList.remove('hide');
-		app.sidebarShow = true;
+/**
+ * [updateTemplate 更新页面
+ * @param  {[type]} data         获取的数据
+ * @param  {[type]} templateName 数据对应模板名称
+ * @return {[type]}              [description]
+ */
+app.updateTemplate = function (data, templateName) {
+  app.hideLoading();
+	if (templateName === 'homeListTemplate') {
+		var container = document.querySelector('.main-list')
+		container.textContent = '';
+		data.forEach(function (item) {
+			var tpl = app.homeListTemplate.cloneNode(true);
+			tpl.querySelector('a').setAttribute('href', item.url);
+			tpl.querySelector('img').setAttribute('src', item.img);
+			tpl.querySelector('.name').textContent = item.name;
+			tpl.querySelector('.price').textContent = '$' + item.price;
+			tpl.querySelector('.decs').textContent = item.description;
+			container.appendChild(tpl);
+		})
 	}
+}
 
-	app.closeSidebar = function () {
-		if (!app.sidebarShow) return;
-		document.querySelector('.mask').classList.add('hide');
-		document.querySelector('.sidebar').classList.add('hide');
-		app.sidebarShow = false;
-	}
 
-	app.showLoading = function () {
-		if (app.loadingShow) return;
-		document.querySelector('.loading').classList.remove('hide');
-		app.loadingShow = true;
-	}
+/*******************
+* 事件监听
+*******************/
 
-	app.hideLoading = function () {
-		if (!app.loadingShow) return;
-		document.querySelector('.loading').classList.add('hide');
-		app.loadingShow = false;
-	}
+// sidebar btn
+document.querySelector('.sidebar').addEventListener('click', function () {
+	app.closeSidebar();
+})
 
-	// 请求数据并更新
-	app.updateTemplate(initialHomeListData.data.list, 'homeListTemplate');
+document.querySelector('.icon-category').addEventListener('click', function () {
+	app.openSidebar();
+})
+
+document.querySelector('.mask').addEventListener('click', function () {
+	app.closeSidebar();
+})
+
+
+document.querySelector('.icon-refresh').addEventListener('click', function () {
+	app.refresh();
+})
+
+
+app.openSidebar = function () {
+	if (app.sidebarShow) return;
+	document.querySelector('.mask').classList.remove('hide');
+	document.querySelector('.sidebar').classList.remove('hide');
+	app.sidebarShow = true;
+}
+
+app.closeSidebar = function () {
+	if (!app.sidebarShow) return;
+	document.querySelector('.mask').classList.add('hide');
+	document.querySelector('.sidebar').classList.add('hide');
+	app.sidebarShow = false;
+}
+
+app.showLoading = function () {
+	if (app.loadingShow) return;
+	document.querySelector('.loading').classList.remove('hide');
+	app.loadingShow = true;
+}
+
+app.hideLoading = function () {
+	if (!app.loadingShow) return;
+	document.querySelector('.loading').classList.add('hide');
+	app.loadingShow = false;
+}
+
+
+// 请求数据并更新
+app.refresh();
 	
-})()
